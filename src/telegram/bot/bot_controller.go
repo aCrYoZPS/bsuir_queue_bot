b9dfac65e2826f04e755903fa773ab7503d2fb20
shosh <piglyr@proton.me>
2025-07-11 20:09:57 +0300

    deleted singletone remains from bot

--- src/telegram/bot/bot_controller.go
+++ src/telegram/bot/bot_controller.go
@@ -1,7 +1,6 @@
 package bot
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/aCrYoZPS/bsuir_queue_bot/src/logging"
@@ -30,34 +29,21 @@ func NewBotController(token string, debug bool, msgSrv MessagesService, callback
 	return bc, nil
 }
 
-func (bc *BotController) GetBot() (*tgbotapi.BotAPI, error) {
-	if bc.bot == nil {
-		return nil, errors.New("bot not initialized")
-	}
-
-	return bc.bot, nil
-}
-
 func (controller *BotController) Start() {
-	bot, err := controller.GetBot()
-	if err != nil {
-		logging.FatalLog("failed to get bot instance", "error", err.Error())
-	}
-
-	logging.Info(fmt.Sprintf("authorized on account %s", bot.Self.UserName))
+	logging.Info(fmt.Sprintf("authorized on account %s", controller.bot.Self.UserName))
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+	updates := controller.bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message != nil {
 			if update.Message.Command() != "" {
-				controller.msgSrv.HandleCommands(&update, bot)
+				controller.msgSrv.HandleCommands(&update, controller.bot)
 			}
 			if update.CallbackQuery != nil {
-				controller.callbackSrv.HandleCallbacks(&update, bot)
+				controller.callbackSrv.HandleCallbacks(&update, controller.bot)
 			}
 		}
 	}
